4123c6a04ff6e67ca349d8437b904cf89c0a13af
Christopher Ruiz <chrisruizaz@gmail.com>
2020-01-09 20:06:02 -0700

    I made some css stuff

--- client/src/pages/Home/index.jsx
+++ client/src/pages/Home/index.jsx
@@ -1,41 +1,39 @@
-import React, { Component } from 'react';
-import { Header } from '../../components';
+import React, { Component } from "react";
+import { Header } from "../../components";
 import "./home.css";
+import { LoginForm } from "../../components";
 
 export default class Home extends Component {
-	constructor(props) {
-		super(props)
-		this.state = {
-			user: props.user
-		}
-	}
+  constructor(props) {
+    super(props);
+    this.state = {
+      user: props.user
+    };
+  }
 
-	componentDidMount() {
-		console.log("Home Component Mounted")
-	}
+  componentDidMount() {
+    console.log("Home Component Mounted");
+  }
 
-	render() {
-		if (this.props.user) {
-			return (
-				<div className="Home">
-					<Header user={this.state.user} />
-					<p>Current User:</p>
-					<code>
-						{JSON.stringify(this.props)}
-					</code>
-				</div>
-			)
-		} else {
-			return (
-				<div className="Home">
-					<Header user={this.state.user} />
-					<p>Current User:</p>
-					<code>
-						{JSON.stringify(this.props)}
-					</code>
-				</div>
-			)
-		}
-	}
-
+  render() {
+    if (this.props.user) {
+      return (
+        <div className="Home">
+          <LoginForm />
+          <Header user={this.state.user} />
+          <p>Current User:</p>
+          <code>{JSON.stringify(this.props)}</code>
+        </div>
+      );
+    } else {
+      return (
+        <div className="Home">
+          <LoginForm />
+          <Header user={this.state.user} />
+          <p>Current User:</p>
+          <code>{JSON.stringify(this.props)}</code>
+        </div>
+      );
+    }
+  }
 }
--- server.js
+++ server.js
@@ -1,71 +1,73 @@
 // Loading evnironmental variables here
-if (process.env.NODE_ENV !== 'production') {
-	console.log('loading dev environments')
-	require('dotenv').config()
+if (process.env.NODE_ENV !== "production") {
+  console.log("loading dev environments");
+  require("dotenv").config();
 }
-require('dotenv').config()
+require("dotenv").config();
 
-const express = require('express')
-const bodyParser = require('body-parser');
-const morgan = require('morgan');
-const session = require('express-session');
-const MongoStore = require('connect-mongo')(session);
-const passport = require('./utils/passport');
-const mongoose = require('mongoose');
-const path = require('path');
-const app = express()
+const express = require("express");
+const bodyParser = require("body-parser");
+const morgan = require("morgan");
+const session = require("express-session");
+const MongoStore = require("connect-mongo")(session);
+const passport = require("./utils/passport");
+const mongoose = require("mongoose");
+const path = require("path");
+const app = express();
 const routes = require("./controllers");
-const PORT = process.env.PORT || 8080
-const io = require('socket.io')();
+const PORT = process.env.PORT || 8080;
+const io = require("socket.io")();
 
-io.on('connection', (client) => {
-  client.on('subscribeToTimer', (interval) => {
-    console.log(client + 'client is subscribing to timer with interval ', interval);
+io.on("connection", client => {
+  client.on("subscribeToTimer", interval => {
+    console.log(
+      client + "client is subscribing to timer with interval ",
+      interval
+    );
     setInterval(() => {
-      client.emit('timer', new Date());
+      client.emit("timer", new Date());
     }, interval);
   });
 });
 
 const socketPort = 8000;
 io.listen(socketPort);
-console.log('listening on port ', socketPort);
-
-
+console.log("listening on port ", socketPort);
 
 // ===== Middleware ====
-app.use(morgan('dev'))
+app.use(morgan("dev"));
 app.use(
-	bodyParser.urlencoded({
-		extended: false
-	})
-)
+  bodyParser.urlencoded({
+    extended: false
+  })
+);
 app.use(bodyParser.json());
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/projectthree",
-	{
-		useNewUrlParser: true,
-		useUnifiedTopology: true
-	});
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhost/projectthree",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  }
+);
 
 app.use(
-	session({
-		secret: process.env.APP_SECRET || 'this is the default passphrase',
-		store: new MongoStore({ mongooseConnection: mongoose.connection }),
-		resave: false,
-		saveUninitialized: false
-	})
-)
+  session({
+    secret: process.env.APP_SECRET || "this is the default passphrase",
+    store: new MongoStore({ mongooseConnection: mongoose.connection }),
+    resave: false,
+    saveUninitialized: false
+  })
+);
 
 // ===== Passport ====
-app.use(passport.initialize())
-app.use(passport.session()) // will call the deserializeUser
-
+app.use(passport.initialize());
+app.use(passport.session()); // will call the deserializeUser
 
 // ==== if its production environment!
 if (process.env.NODE_ENV === "production") {
-	console.log("Prod Mode Enabled")
-	app.use(express.static("client/build"));
+  console.log("Prod Mode Enabled");
+  app.use(express.static("client/build"));
 }
 
 // ====== Routing & Controllers =====
@@ -73,27 +75,25 @@ app.use(routes);
 
 // ====== React App ======
 app.get("*", (req, res) => {
-	res.sendFile(path.join(__dirname, "./client/build/index.html"));
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
 
 // ====== Error handler ====
-app.use(function (err, req, res, next) {
-	console.log('====== ERROR =======')
-	console.error(err.stack)
-	res.status(500)
-})
-
-
+app.use(function(err, req, res, next) {
+  console.log("====== ERROR =======");
+  console.error(err.stack);
+  res.status(500);
+});
 
 // ==== Starting Server =====
 
-io.on('connect', function (socket) {
-	console.log('a user connected');
-	socket.on('disconnect', function () {
-	  console.log('user disconnected');
-	});
+io.on("connect", function(socket) {
+  console.log("a user connected");
+  socket.on("disconnect", function() {
+    console.log("user disconnected");
   });
+});
 
 app.listen(PORT, () => {
-	console.log(`App listening on PORT: ${PORT}`)
-})
+  console.log(`App listening on PORT: ${PORT}`);
+});
