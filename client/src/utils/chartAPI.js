bf3298542f96782332423d5e12e1c01401073f15
Aaron Beck <aaronmicalbeck@gmail.com>
2020-01-18 12:37:01 -0700

    deleted unnecessary route in chartAPI

--- client/src/utils/chartAPI.js
+++ client/src/utils/chartAPI.js
@@ -1,14 +1,9 @@
 import axios from "axios";
 
 export default {
-  // Gets all Google Users
+  // Gets all Users
   getGoogleUser: function() {
     return axios.get("/api/userScore");
   },
 
-  // Gets all Local Users
-
-  // getLocalUser: function(){
-  //   return axios.get("/api/userScore");
-  // }
 };
